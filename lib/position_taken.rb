23372230913f034f35e571e66b269af233246d31
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-01-11 00:16:30 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,5 @@
 # code your #index_taken? method here!
 def index_taken?(board, index)
-  # if board[index] == " " || board[index] == "" || board[index] == nill
-  #   false
   if board[index] == "X" || board[index] == "O"
   true
   end
